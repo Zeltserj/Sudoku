17d8e4996d60229d77c1fb6380562d3887621daa
ornechemia <nechemiaor@gmail.com>
2019-07-29 16:59:08 +0300

    added linkedListCells

---
+++ linkedListCells.c
@@ -0,0 +1,2 @@
+
+#include "linkedListCells.h"
---
+++ linkedListCells.h
@@ -0,0 +1,11 @@
+
+#ifndef SP_LINKEDLISTCELLS_H
+#define SP_LINKEDLISTCELLS_H
+
+typedef struct LinkedListCells{
+
+}LinkedListCells;
+
+void add();
+
+#endif
