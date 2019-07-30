37b7660c091814f9a4ad21c64cbd790e895ff7ec
ornechemia <nechemiaor@gmail.com>
2019-07-30 16:47:02 +0300

    stack.c/.h: added pop, push, peek, alloc, free, add, is_empty

--- stack.c
+++ stack.c
@@ -1,2 +1,59 @@
 
 #include "stack.h"
+
+
+Stack * alloc_stack() {
+    Stack* newSrack = calloc(1,sizeof(int*));
+    if(newSrack==NULL){
+        error("stack","alloc_stack",1);
+        exit(0);
+    }
+}
+
+StackNode *alloc_stack_node(int row, int col, int value) {
+    StackNode* node = calloc(4, sizeof(int*));
+    if(node == NULL){
+        error("stack","alloc_stack_node",1);
+        exit(0);
+    }
+    node->row=row;
+    node->col=col;
+    node->value=value;
+}
+
+void push(Stack *stack, StackNode *node) {
+    StackNode* temp= stack->head;
+    stack->head = node;
+    node->next = temp;
+}
+
+StackNode *pop(Stack *stack) {
+    StackNode* temp=stack->head;
+    stack->head=stack->head->next;
+    return temp;
+}
+
+StackNode *peek(Stack *stack) {
+    return stack->head;
+}
+
+int is_empty_stack(Stack *stack) {
+    return (stack->head==NULL);
+}
+
+void free_stack_node(StackNode *node) {
+    free(node->next);
+    free(node);
+}
+
+void free_stack(Stack* stack) {
+    StackNode* temp = peek(stack);
+    while(temp!=NULL && !is_empty_stack(stack)){
+        temp = pop(stack);
+        free(temp);
+    }
+    free(stack);
+}
+
+
+
--- stack.h
+++ stack.h
@@ -2,4 +2,28 @@
 #ifndef SP_STACK_H
 #define SP_STACK_H
 
+#include <stdlib.h>
+#include "errors.h"
+
+typedef struct StackNode{
+    int row, col, value;
+    struct StackNode* next;
+}StackNode;
+
+typedef struct Stack{
+    StackNode* head;
+}Stack;
+
+Stack * alloc_stack();
+StackNode *alloc_stack_node(int row, int col, int value);
+
+void push(Stack *stack, StackNode *node);
+StackNode* pop(Stack *stack);
+StackNode* peek(Stack *stack);
+
+int is_empty_stack(Stack *stack);
+
+void free_stack_node(StackNode *node);
+void free_stack(Stack* stack);
+
 #endif /*SP_STACK_H*/
