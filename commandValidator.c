bb36f5be1862712a74c199a37af93637f4bbf9cf
Zeltserj <47822262+Zeltserj@users.noreply.github.com>
2019-07-30 16:42:35 +0300

    added command validator modules, fixed print board

---
+++ commandValidator.c
@@ -0,0 +1,53 @@
+/**
+
+ Created by lenovo on ${DTE}
+ **/
+
+
+#include "commandValidator.h"
+
+int validate_command(Command *command) {
+    int out = 1;
+    switch (command->type){
+        case INVALID:
+            user_error(6);
+            out = 0;
+            break;
+        case SOLVE:
+            break;
+        case EDIT:
+            break;
+        case MARK_ERRORS:
+            break;
+        case PRINT_BOARD:
+            break;
+        case SET:
+            break;
+        case VALIDATE:
+            break;
+        case GUESS:
+            break;
+        case GENERATE:
+            break;
+        case UNDO:
+            break;
+        case REDO:
+            break;
+        case SAVE:
+            break;
+        case HINT:
+            break;
+        case GUESS_HINT:
+            break;
+        case NUM_SOLUTIONS:
+            break;
+        case AUTOFILL:
+            break;
+        case RESET:
+            break;
+        case EXIT:
+            break;
+    }
+    return 0;
+}
+
