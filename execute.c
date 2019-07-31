7c108726da0b4bf64367804c54588abe23393e23
ornechemia <nechemiaor@gmail.com>
2019-07-31 15:28:58 +0300

    linkedListCell.c/.h: added advance_curr_cell execute.c/.h: started undo :)

--- execute.c
+++ execute.c
@@ -12,8 +12,11 @@ int execute_command(Command *command) {
         case VALIDATE:break;
         case GUESS:break;
         case GENERATE:break;
-        case UNDO:break;
-        case REDO:break;
+        case UNDO:
+
+            break;
+        case REDO:
+            break;
         case SAVE:break;
         case HINT:break;
         case GUESS_HINT:break;
@@ -24,6 +27,25 @@ int execute_command(Command *command) {
     }
 }
 
-void set_mode(int newmode) {
-     mode = newmode;
+void set_mode(int new_mode) {
+     mode = new_mode;
+}
+
+void mark_errors_command(int mark) {
+    mark_errors = mark;
+}
+
+void undo(Board *board, LinkedList *moves) {
+    Node* temp = moves->current;
+    LinkedListCells* cells = temp->changed;
+    backward_curr(moves);
+
+}
+void change_cells_to(Board *board, LinkedListCells *old_values) {
+    int i;
+    move_curr_to_head(old_values);
+    for(i=0;i<old_values->len;i++){
+        cell_cpy(old_values->current);
+        advance_curr_cell(old_values);
+    }
 }
--- execute.h
+++ execute.h
@@ -2,9 +2,21 @@
 #ifndef SPROJECT_EXECUTE_H
 #define SPROJECT_EXECUTE_H
 #include "command.h"
+#include "linkedList.h"
 
 int execute_command(Command* command);
-void set_mode(int mode);
+void set_mode(int new_mode);
 
+/**
+* undo the last move and change the cells of board respectively back to the values before that move
+* @param board != NULL
+* @param moves != NULL. A list of moves containd SET/ AUTOFILL/ GENERATE/ GUESS moves.
+* move.current is the last move executed on board
+*/
+void undo(Board *board, LinkedList *moves);
+
+void redo(Board *board, LinkedList *moves);
+void mark_errors_command(int mark);
+void print_board_command(Board* board);
 
 #endif /*SPROJECT_EXECUTE_H*/
