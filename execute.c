d631814a67e381b4bd9c6eaece8d7844be890dbb
ornechemia <nechemiaor@gmail.com>
2019-08-01 16:47:54 +0300

    execute.c: change_cells_to: set_cell instead of copy_cell :)

--- execute.c
+++ execute.c
@@ -71,7 +71,7 @@ void change_cells_to(Board *board, LinkedListCells *old_values) {
     int i;
     move_curr_to_head(old_values);
     for(i=0;i<old_values->len;i++){
-        cell_copy(board,old_values->current);
+        set_cell(board,old_values->current->c);
         advance_curr_cell(old_values);
     }
 }
